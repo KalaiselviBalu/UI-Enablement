21c620271c03ff79a5b7684b59e1821ea7c2d079
KalaiselviBalu <kalaiselvi.balu@siriuscom.com>
2022-08-18 12:17:35 +0530

    calculator:initial commit

---
+++ calculator/assets/js/calculator.js
@@ -0,0 +1,86 @@
+// Method 1 //
+
+// let calculator = {
+//     addition : {
+//         num1 : 4,
+//         num2 : 4
+//     },
+//     subtraction : {
+//         num1 : 4,
+//         num2 : 4
+//     },
+//     multiplication : {
+//         num1 : 4,
+//         num2 : 4
+//     },
+//     division : {
+//         num1 : 4,
+//         num2 : 4
+//     },
+// }
+
+// let result;
+
+// function calculate(operation) {
+  
+//     if (operation == "addition"){
+//        result = calculator.add.num1 + calculator.add.num2;
+//        console.log(result);
+//     }
+//     else if (operation == "subtraction"){
+//         result = calculator.subtraction.num1 - calculator.subtraction.num2;
+//         console.log(result);
+//      }
+//      else if (operation == "multiplication"){
+//         result = calculator.multiplication.num1 * calculator.multiplication.num2;
+//         console.log(result);
+//      }
+//      else if (operation == "division"){
+//         result = calculator.division.num1 / calculator.division.num2;
+//         console.log(result);
+//      }
+// }
+
+// calculate("division");
+
+
+
+
+// Method 2 //
+
+// const num1 = parseInt(prompt('Enter the first number'));
+// const num2 = parseInt(prompt('Enter the second number'));
+
+// let calculator = {
+//    addition : "",
+//    subtraction : "",
+//    multiplication : "",
+//    division:""
+// }
+
+// let result;
+
+// function calculate(operation) {
+  
+//     if (operation == "addition"){
+//        result = num1 + num2;
+//        console.log(result);
+//     }
+//     if (operation == "subtraction"){
+//         result = num1 - num2;
+//         console.log(result);
+//      }
+//      if (operation == "multiplication"){
+//         result = num1 * num2;
+//         console.log(result);
+//      }
+//      if (operation == "division"){
+//         result = num1 / num2;
+//         console.log(result);
+//      }
+   
+// }
+
+// calculate("multiplication");
+
+
