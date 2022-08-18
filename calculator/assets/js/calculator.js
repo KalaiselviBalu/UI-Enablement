eac7c01eeeeba9e134145151833de9fc8a3899e9
KalaiselviBalu <kalaiselvi.balu@siriuscom.com>
2022-08-18 18:26:10 +0530

    calculator : eview comments commit

--- calculator/assets/js/calculator.js
+++ calculator/assets/js/calculator.js
@@ -1,86 +1,30 @@
-// Method 1 //
 
-// let calculator = {
-//     addition : {
-//         num1 : 4,
-//         num2 : 4
-//     },
-//     subtraction : {
-//         num1 : 4,
-//         num2 : 4
-//     },
-//     multiplication : {
-//         num1 : 4,
-//         num2 : 4
-//     },
-//     division : {
-//         num1 : 4,
-//         num2 : 4
-//     },
-// }
+//Method 1
+const calculator = {
+add : function(num1 , num2){
+    this.num1 = num1;
+    this.num2 = num2;
+    console.log(this.num1 + this.num2)
+},
+sub : function(num1 , num2){
+    this.num1 = num1;
+    this.num2 = num2;
+    console.log(this.num1 - this.num2)
+},
+multiply : function(num1 , num2){
+    this.num1 = num1;
+    this.num2 = num2;
+    console.log(this.num1 * this.num2)
+},
+division : function(num1 , num2){
+    this.num1 = num1;
+    this.num2 = num2;
+    console.log(this.num1 / this.num2)
+}
+}
 
-// let result;
-
-// function calculate(operation) {
-  
-//     if (operation == "addition"){
-//        result = calculator.add.num1 + calculator.add.num2;
-//        console.log(result);
-//     }
-//     else if (operation == "subtraction"){
-//         result = calculator.subtraction.num1 - calculator.subtraction.num2;
-//         console.log(result);
-//      }
-//      else if (operation == "multiplication"){
-//         result = calculator.multiplication.num1 * calculator.multiplication.num2;
-//         console.log(result);
-//      }
-//      else if (operation == "division"){
-//         result = calculator.division.num1 / calculator.division.num2;
-//         console.log(result);
-//      }
-// }
-
-// calculate("division");
+const calcObj = new calculator.division(2,2);
 
 
-
-
-// Method 2 //
-
-// const num1 = parseInt(prompt('Enter the first number'));
-// const num2 = parseInt(prompt('Enter the second number'));
-
-// let calculator = {
-//    addition : "",
-//    subtraction : "",
-//    multiplication : "",
-//    division:""
-// }
-
-// let result;
-
-// function calculate(operation) {
-  
-//     if (operation == "addition"){
-//        result = num1 + num2;
-//        console.log(result);
-//     }
-//     if (operation == "subtraction"){
-//         result = num1 - num2;
-//         console.log(result);
-//      }
-//      if (operation == "multiplication"){
-//         result = num1 * num2;
-//         console.log(result);
-//      }
-//      if (operation == "division"){
-//         result = num1 / num2;
-//         console.log(result);
-//      }
-   
-// }
-
-// calculate("multiplication");
 
 
