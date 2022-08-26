4b99acf18d621b8886768315fe6a905a1d01324b
KalaiselviBalu <kalaiselvi.balu@siriuscom.com>
2022-08-26 21:03:06 +0530

    pocket-notes : initial commit

---
+++ pocket-notes/scripts/script.js
@@ -0,0 +1,11 @@
+function addNote(){
+    var wrapper = document.querySelector(".wrapper");
+    wrapper.classList.add("overlay");
+    var modal = document.querySelector(".modal");
+    modal.classList.add("show");
+
+   return function validateModal() {
+        alert("hi");
+
+    }
+}
